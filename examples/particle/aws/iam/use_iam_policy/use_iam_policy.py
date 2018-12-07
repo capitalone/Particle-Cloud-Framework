866e884058590f8e8f7e46e299315c7140a20fac
David Yun <davidyun@Davids-MBP.fios-router.home>
2018-12-07 16:48:57 -0500

    iam policy update added

--- examples/particle/aws/iam/use_iam_policy/use_iam_policy.py
+++ examples/particle/aws/iam/use_iam_policy/use_iam_policy.py
@@ -4,22 +4,40 @@ import json
 
 
 my_managed_policy = {
-    "Version": "2012-10-17",
-    "Statement": [
+    'Version': '2012-10-17',
+    'Statement': [
         {
-            "Effect": "Allow",
-            "Action": "logs:CreateLogGroup",
-            "Resource": "*"
+            'Effect': 'Allow',
+            'Action': 'logs:CreateLogGroup',
+            'Resource': '*'
         },
         {
-            "Effect": "Allow",
-            "Action": [
-                "dynamodb:DeleteItem",
-                "dynamodb:GetItem",
-                "dynamodb:PutItem",
-                "dynamodb:Scan",
+            'Effect': 'Allow',
+            'Action': [
+                'dynamodb:DeleteItem',
+                'dynamodb:GetItem',
+                'dynamodb:PutItem',
             ],
-            "Resource": "*"
+            'Resource': '*'
+        }
+    ]
+}
+
+update_managed_policy = {
+    'Version': '2012-10-17',
+    'Statement': [
+        {
+            'Effect': 'Allow',
+            'Action': 'logs:CreateLogGroup',
+            'Resource': '*'
+        },
+        {
+            'Effect': 'Allow',
+            'Action': [
+                'dynamodb:DeleteItem',
+                'dynamodb:GetItem',
+            ],
+            'Resource': '*'
         }
     ]
 }
@@ -29,15 +47,31 @@ iam_policy_example_json = {
     "pcf_name": "pcf_iam_policy", # Required
     "flavor":"iam_policy", # Required
     "aws_resource":{
-        # Refer to https://boto3.readthedocs.io/en/latest/reference/services/s3.html#S3.Client.create_bucket for full list of parameters
         "PolicyName":"pcf-test", # Required
         "PolicyDocument": json.dumps(my_managed_policy)
     }
 }
 
-# create S3 Bucket particle
+# create IAM Policy particle
 policy = IAMPolicy(iam_policy_example_json)
 
 # example start
 policy.set_desired_state(State.running)
 policy.apply()
+print(policy.state)
+print(policy.current_state_definition)
+
+# example update
+iam_policy_example_json['aws_resource']['PolicyDocument'] = json.dumps(update_managed_policy)
+policy = IAMPolicy(iam_policy_example_json)
+policy.set_desired_state(State.running)
+policy.apply()
+print(policy.current_state_definition)
+
+# example update
+policy.set_desired_state(State.terminated)
+policy.apply()
+print(policy.state)
+
+
+
