3ece9f7040d545f110f9b5d494bfa21fe3c8e53d
Brian Lee <brian.lee@capitalone.com>
2019-01-09 11:37:06 -0500

    import cloudfront

--- examples/particle/aws/cloudfront/use_cloudfront/example_cloudfront.py
+++ examples/particle/aws/cloudfront/use_cloudfront/example_cloudfront.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from pcf.particle.aws.cloudfront.cloudfront import CloudFront
 from pcf.core import State
 import random
 import string
