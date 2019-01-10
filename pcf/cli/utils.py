ad6f781123e650293ffc22985305da960397138e
jacoblearned <jacob@scientismist.com>
2019-01-10 11:06:01 -0500

    Add top-level pcf command

--- pcf/cli/utils.py
+++ pcf/cli/utils.py
@@ -4,11 +4,13 @@ import os
 import sys
 import click
 
+
 def no_color():
     """ Determine if user has set the NO_COLOR environment variable to any value
         https://no-color.org
     """
-    return bool(os.envion.get('NO_COLOR'))
+    return bool(os.envion.get("NO_COLOR"))
+
 
 def color(color):
     """ Return the desired color name or None if the NO_COLOR env var is set """
