e7aa965683abb861336922586f1f699544e5609a
Brian Lee <brian.lee@capitalone.com>
2019-01-16 15:17:04 -0500

    autogenerate test for particle classes
    example is of a hosted zone
    
    can add the rest once I get the good to go

--- pcf/test/test_particles.py
+++ pcf/test/test_particles.py
@@ -1,77 +1,19 @@
 import moto
+import json
 import pytest
 from pcf.core import State
 from pcf.core import particle_flavor_scanner
 
 from pcf.particle.aws.route53 import hosted_zone
 
-testdata = {
-    "hosted-zone-apply":
-        (
-            {
-                "pcf_name": "pcf_hosted_zone",
-                "flavor": "route53_hosted_zone",
-                "aws_resource": {
-                    "Name": "www.hoooooos.com.",
-                    "custom_config": {
-                        "Tags": [
-                            {
-                                "Key": "Owner",
-                                "Value": "Hoo"
-                            },
-                            {
-                                "Key": "UID",
-                                "Value": "abc123"
-                            }
-                        ]
-                    },
-                    "VPC": {
-                        "VPCRegion": "us-east-1",
-                        "VPCId": "vpc-12345"
-                    },
-                    "CallerReference": "werhasdkfboi12hasdfak",
-                    "HostedZoneConfig": {
-                        "Comment": "hoo",
-                        "PrivateZone": True
-                    },
-                    # "DelegationSetId": ""
-                }
-            },
-            {
-                "pcf_name": "pcf_hosted_zone",
-                "flavor": "route53_hosted_zone",
-                "aws_resource": {
-                    "Name": "www.hoooooos.com.",
-                    "custom_config": {
-                        "Tags": [
-                            {
-                                "Key": "Owner",
-                                "Value": "Hoo"
-                            },
-                            {
-                                "Key": "UID",
-                                "Value": "abc123"
-                            }
-                        ]
-                    },
-                    "VPC": {
-                        "VPCRegion": "us-east-1",
-                        "VPCId": "vpc-12345"
-                    },
-                    "CallerReference": "werhasdkfboi12hasdfak",
-                    "HostedZoneConfig": {
-                        "Comment": "hoo",
-                        "PrivateZone": True
-                    },
-                    # "DelegationSetId": ""
-                }
-            },
-            "mock_route53"
-        ),
-}
+with open("testdata.json") as f:
+    testdata = json.load(f)
+
+values = testdata.values()
+values = [tuple(v) for v in values]
 
 
-@pytest.mark.parametrize("definition,updated_definition,mototag", list(testdata.values()), ids=list(testdata.keys()))
+@pytest.mark.parametrize("definition,updated_definition,mototag", values, ids=list(testdata.keys()))
 def test_apply(definition, updated_definition, mototag):
     flavor = definition.get("flavor")
     particle_class = particle_flavor_scanner.get_particle_flavor(flavor)
@@ -93,4 +35,3 @@ def test_apply(definition, updated_definition, mototag):
         particle.apply()
 
         assert particle.get_state() == State.terminated
-
