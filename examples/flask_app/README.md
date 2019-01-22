b2168f88b26c4d6c7970f46b44de8b8b3d040045
David Yun <davidyun@Davids-MBP.fios-router.home>
2019-01-22 04:26:58 -0500

    remove higgs reference

--- examples/flask_app/README.md
+++ examples/flask_app/README.md
@@ -1,10 +1,11 @@
 # PCF-Flask-API
-The purpose of this app is to show how to use PCF in a Flask api to spin up cloud resources. In this particular example we will utilize Capital One's Particle Cloud Framework to deploy ec2-route53 quasiparticle.
+- The purpose of this example is to show one of multiple ways to use Particle Cloud Framework (PCF) to deploy cloud infrastructure which in this case is using a Flask api. In this particular example we deploy the ec2-route53 quasiparticle which creates a route53 record and adds the desired number on ec2 instances to the record set. To use in your environment, simply change some of the configurations parameters in the quasiparticle definition.
+
 
 
 ##
 
-Run Flask App Locally with Docker
+Interact with Flask App Locally using Docker
 ------------
 
 `docker build -t pcf_flask_api .`
@@ -15,23 +16,23 @@ Run Flask App Locally with Docker
 Run Flask App Locally with Docker
 ------------
 Once we have the flask app running locally, we can hit the endpoint to spin up the quasiparticle. 
-First, set the desired configuration in higgs_example_config.json file.
+First, set the desired configuration in pcf_example_config.json file.
 
 To spin up the ec2-route53 quasiparticle with the desired configuration:
 
-`curl -X POST http://localhost:5000/higgs`
+`curl -X POST http://localhost:5000/pcf`
 
 
 To get the state of the ec2-route53 quasiparticle:
 
-`curl -X GET http://localhost:5000/higgs`
+`curl -X GET http://localhost:5000/pcf`
 
 
 To spin down the ec2-route53 quasiparticle:
 
-`curl -X DELETE http://localhost:5000/higgs`
+`curl -X DELETE http://localhost:5000/pcf`
 
 
 To do an update, make a POST request after setting the new desired configuration:
 
-`curl -X POST http://localhost:5000/higgs`
+`curl -X POST http://localhost:5000/pcf`
--- examples/flask_app/app.py
+++ examples/flask_app/app.py
@@ -1,28 +1,33 @@
 import json
 import pcf
 
-from flask import Flask
+from flask import Flask, request
 from pcf.core import State
 from pcf.quasiparticle.aws.ec2_route53.ec2_route53 import EC2Route53
 
 app = Flask(__name__)
 
 #We read the json configuration of our desired quasiparticle
-with open('higgs_example_config.json', 'r') as f:
-    higgs_config = json.load(f)
+with open('pcf_example_config.json', 'r') as f:
+    pcf_config = json.load(f)
 
 #Here we initialize the desired state definitions
-ec2_route53_quasiparticle = EC2Route53(higgs_config)
 
-#GET /higgs endpoint returns the current state of our EC2 Route53 Quasiparticle
-@app.route('/higgs', methods=['GET'])
-def get_higgs_status():
+#GET /pcf endpoint returns the current state of our EC2 Route53 Quasiparticle
+@app.route('/pcf', methods=['GET'])
+def get_pcf_status():
     return str(ec2_route53_quasiparticle.get_state())
 
-#POST /higgs endpoint creates the EC2 Route53 Quasiparticle with the desired configuration
-@app.route('/higgs', methods=['POST'])
+#POST /pcf endpoint creates the EC2 Route53 Quasiparticle with the desired configuration
+@app.route('/pcf', methods=['POST'])
 def create():
     ec2_route53_quasiparticle.set_desired_state(State.running)
+
+    if request.data:
+        payload = json.loads(request.data)
+        multiplier = payload.get('ec2_multiplier')
+        pcf_config['particles'][0]['multiplier'] = multiplier
+        ec2_route53_quasiparticle = EC2Route53(pcf_config)
 
     try:
         ec2_route53_quasiparticle.apply(sync=True)
@@ -31,8 +36,8 @@ def create():
 
     return str(ec2_route53_quasiparticle.get_state())
 
-#DELETE /higgs endpoint deletes the EC2 Route53 Quasiparticle
-@app.route('/higgs', methods=['DELETE'])
+#DELETE /pcf endpoint deletes the EC2 Route53 Quasiparticle
+@app.route('/pcf', methods=['DELETE'])
 def delete():
     ec2_route53_quasiparticle.set_desired_state(State.terminated)
 
