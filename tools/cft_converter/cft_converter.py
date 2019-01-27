715b31b1ce74bb1427e0355ad323e2bd4fc924ff
anovis <anovis@ymail.com>
2019-01-26 19:12:04 -0500

    base classes and property converter

--- tools/cft_converter/cft_converter.py
+++ tools/cft_converter/cft_converter.py
@@ -1,20 +1,43 @@
-
-class Field:
-    def __init__(self, cft, cft_key):
-        self.extarcted_value = self.extract(cft,cft_key)
-        self.pcf_obj = self.conversion(self.extarcted_value)
-
-    def extract(self,cft, cft_tag):
-        for key,value in cft.items():
-            if key == cft_tag:
-                return value
-
-    def conversion(self,cft_value):
-        return cft_value
+base_particle = {
+    "pcf_name":"PCF_NAME",
+    "flavor":"FLAVOR",
+    "parents":[],
+    "aws_resource":{}
+}
 
 
-class ParticleField(Field):
-    def __init__(self):
-        self.aws_resource = Field(self.cft,"Properties")
-        self.pcf_name = Field()
-        self.flavor = Field()
+class Conversion:
+
+    cft_field = None
+    pcf_field = None
+
+    def __init__(self, cft_def, particle_def):
+        self.cft_def = cft_def
+        self.particle_def = particle_def
+
+    def convert(self):
+        self.particle_def[self.__class__.pcf_field] = self.cft_def[self.__class__.cft_field]
+        return self.particle_def
+
+
+class PropertyConversion(Conversion):
+
+    cft_field="Properties"
+    pcf_field="aws_resource"
+
+
+class ConvertResource:
+
+    CONVERSIONS= [
+        PropertyConversion
+    ]
+
+    def __init__(self,cft_resource):
+        self.particle = base_particle
+        self.cft = cft_resource
+
+    def convert_fields(self, conversions="all"):
+        for conversion in self.CONVERSIONS:
+            if conversion.cft_field in conversions or conversions == 'all':
+                self.particle = conversion(self.cft, self.particle).convert()
+
