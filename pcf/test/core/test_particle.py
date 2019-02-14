096a22ef517017416ca65b726924a70acfa9fa76
David Yun <davidyun@Davids-MacBook-Pro.local>
2019-02-14 15:04:48 -0500

    add test for list var  parent-child pass through

--- pcf/test/core/test_particle.py
+++ pcf/test/core/test_particle.py
@@ -81,7 +81,7 @@ class ParticlePassingVars(Particle):
 
     def sync_state(self):
         if self.name == "parent":
-            self.current_state_definition = {"item":"var_to_be_passed", "nested":{"nested_key":"nested_var"}}
+            self.current_state_definition = {"item":"var_to_be_passed", "item2":"var2_to_be_passed", "nested":{"nested_key":"nested_var"}}
             self.desired_state_definition = self.current_state_definition
         self.state = State.running
         self.current_state_definition = self.desired_state_definition
@@ -107,9 +107,9 @@ def test_passing_vars():
         "parents":["particle_flavor_passing_vars:parent"],
         "parent_var":"$inherit$particle_flavor_passing_vars:parent$item",
         "nested_parent_var":"$inherit$particle_flavor_passing_vars:parent$nested.nested_key",
+        "parent_list_var":["$inherit$particle_flavor_passing_vars:parent$item", "$inherit$particle_flavor_passing_vars:parent$item2"],
         "no_parent_exists":"$inherit$particle_flavor_passing_vars:does_not_exist$key",
         "no_flavor_exists":"$inherit$does_not_exist:parent$key",
-        "parent_list_var":["$inherit$particle_flavor_passing_vars:parent$item", "$inherit$particle_flavor_passing_vars:parent$item2"],
         "no_key_exists":"$inherit$particle_flavor_passing_vars:parent$no_key",
         "aws_resource": {
             "resource_name": "some service"
@@ -134,7 +134,7 @@ def test_passing_vars():
     assert(quasiparticle.get_particle("particle_flavor_passing_vars","child").particle_definition["nested_parent_var"] == "nested_var")
     assert(quasiparticle.get_particle("particle_flavor_passing_vars","child").particle_definition["no_parent_exists"] == "$inherit$particle_flavor_passing_vars:does_not_exist$key")
     assert(quasiparticle.get_particle("particle_flavor_passing_vars","child").particle_definition["no_flavor_exists"] == "$inherit$does_not_exist:parent$key")
-    assert(quasiparticle.get_particle("particle_flavor_passing_vars","child").particle_definition["parent_list_var"] == ["$inherit$particle_flavor_passing_vars:parent$item", "$inherit$particle_flavor_passing_vars:parent$item2"])
+    assert(quasiparticle.get_particle("particle_flavor_passing_vars","child").particle_definition["parent_list_var"] == ["var_to_be_passed", "var2_to_be_passed"])
     assert(quasiparticle.get_particle("particle_flavor_passing_vars","child").particle_definition["no_key_exists"] == "$inherit$particle_flavor_passing_vars:parent$no_key")
 
 
