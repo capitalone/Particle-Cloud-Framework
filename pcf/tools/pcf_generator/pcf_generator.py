107b3ca38e55482fa81685dc64404cb79604d9ee
anovis <anovis@ymail.com>
2019-02-15 04:29:11 -0500

    working for single particle

--- pcf/tools/pcf_generator/pcf_generator.py
+++ pcf/tools/pcf_generator/pcf_generator.py
@@ -1,21 +1,24 @@
 import json
-from pcf.util.pcf_util import update_dict
+import os
+from pcf.util.pcf_util import update_dict,particle_class_from_flavor
 
 
 class GenerateParticle:
     def __init__(self, particle_definition):
-        # import particle class
-        self.particle = flavor_class(particle_definition)
+        self.particle_class = particle_class_from_flavor(particle_definition.get("flavor"))
+        self.particle = self.particle_class(particle_definition)
         self.particle_json = particle_definition
 
     def generate_definition(self):
         self.particle.sync_state()
         # TODO generic for all resources
         # TODO desired definition is not always the same format as the current_definition
-        self.particle_json["aws_resource"]= update_dict(self.particle.desired_definkition,self.particle.current_definition)
+        self.particle_json["aws_resource"]= update_dict(self.particle.desired_state_definition,self.particle.current_state_definition)
         return self.particle_json
 
-    def generate_json_file(self, path="", filename='pcf.json'):
+    def generate_json_file(self, path=None, filename='pcf.json'):
+        if not path:
+            path = os.path.dirname((os.path.abspath(__file__)))
         particle_definition = self.generate_definition()
         with open(f'{path}/{filename}', 'w') as file:
             json.dump(particle_definition, file)
