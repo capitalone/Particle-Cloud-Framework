be285bcd99f3baa7e88c9efef2c8792f4f5bccbb
emr0327 <emr0327@aol.com>
2019-03-19 11:09:07 -0400

    Created example for the glacier vault and included a README file. Deleted the galcier test file test.txt because it is not being used.

---
+++ examples/particle/aws/glacier/use_glacier/example_glacier_vault.py
@@ -0,0 +1,38 @@
+from pcf.particle.aws.glacier.glacier_vault import GlacierVault
+from higgs.core import State
+
+# Edit example json to work in your account
+glacier_example_json = {
+    "pcf_name": "pcf_glacier",
+    "flavor": "glacier",
+    "aws_resource": {
+        "vaultName": "pcf_test_glacier", # Required
+        "custom_config": {
+            "Tags": {
+                "Name":"pcf-glacier-example"
+            }
+        }
+    }
+}
+
+# create Glacier particle
+glacier_particle = GlacierVault(glacier_example_json)
+
+# example start
+glacier_particle.set_desired_state(State.running)
+glacier_particle.apply()
+
+print(glacier_particle.get_state())
+
+# show tags
+if glacier_example_json.get("aws_resource").get("custom_config").get("Tags"):
+    tags = glacier_particle.client.list_tags_for_vault(
+            vaultName=glacier_particle.vault_name
+        )
+    print("Tags: ", tags.get("Tags"))
+
+# example terminate
+glacier_particle.set_desired_state(State.terminated)
+glacier_particle.apply()
+
+print(glacier_particle.get_state())
