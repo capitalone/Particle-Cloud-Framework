1e1184a5a6578aff9f32e32719b4c288c4870e66
emr0327 <emr0327@aol.com>
2019-05-09 14:45:14 -0400

    Fleshed out the correct way to write a Placebo test file. Test is idling in the apply while loop trying to update.

--- pcf/test/particle/aws/sns/test_sns_topic.py
+++ pcf/test/particle/aws/sns/test_sns_topic.py
@@ -17,82 +17,88 @@ import boto3
 import os
 
 from pcf.particle.aws.sns.sns_topic import SNSTopic
-PATH = "replay"
-PARTICLE = SNSTopic
-DEF = {
-        "pcf_name": "pcf_sns_test",  # Required
-        "flavor": "sns_topic",  # Required
-        "aws_resource": {
-            # Refer to https://boto3.amazonaws.com/v1/documentation/api/1.9.5/reference/services/sns.html#SNS.Client.create_topic for more information
-            "Name": "pcf-sns-test",  # Required
-            "Attributes": {  # optional - attributes will be set to default values if not specified
-                # "DeliveryPolicy": "", # HTTP|HTTPS|Email|Email-JSON|SMS|Amazon SQS|Application|AWS Lambda
-                "DisplayName": "pcf-test"
-                # "Policy": "" # dict
-            },
-            "custom_config": {
-                # Refer to https://boto3.amazonaws.com/v1/documentation/api/1.9.5/reference/services/sns.html#SNS.Client.subscribe for a full list of parameters
-                # "Subscription": { # optional
-                #     "Protocol": "", # Required - http|https|email|email-json|sms|sqs|application|lambda
-                #     "Endpoint": "", # http|https|email|email-json|sms|sqs|application|lambda
-                #     "Attributes": {}, # dict
-                #     # "ReturnSubscriptionArn": false # boolean - default=false
-                # }
+
+class TestSNSTopic:
+    PATH = "replay"
+    PARTICLE = SNSTopic
+    DEF = {
+            "pcf_name": "pcf_sns_test",  # Required
+            "flavor": "sns_topic",  # Required
+            "aws_resource": {
+                # Refer to https://boto3.amazonaws.com/v1/documentation/api/1.9.5/reference/services/sns.html#SNS.Client.create_topic for more information
+                "Name": "pcf-sns-test",  # Required
+                "Attributes": {  # optional - attributes will be set to default values if not specified
+                    # "DeliveryPolicy": "", # HTTP|HTTPS|Email|Email-JSON|SMS|Amazon SQS|Application|AWS Lambda
+                    "DisplayName": "pcf-test"
+                    # "Policy": "" # dict
+                },
+                "custom_config": {
+                    # Refer to https://boto3.amazonaws.com/v1/documentation/api/1.9.5/reference/services/sns.html#SNS.Client.subscribe for a full list of parameters
+                    # "Subscription": { # optional
+                    #     "Protocol": "", # Required - http|https|email|email-json|sms|sqs|application|lambda
+                    #     "Endpoint": "", # http|https|email|email-json|sms|sqs|application|lambda
+                    #     "Attributes": {}, # dict
+                    #     # "ReturnSubscriptionArn": false # boolean - default=false
+                    # }
+                }
             }
         }
-    }
-UPDATED_DEF = {
-        "pcf_name": "pcf_sns_test",  # Required
-        "flavor": "sns_topic",  # Required
-        "aws_resource": {
-            # Refer to https://boto3.amazonaws.com/v1/documentation/api/1.9.5/reference/services/sns.html#SNS.Client.create_topic for more information
-            "Name": "pcf-sns-test",  # Required
-            "Attributes": {  # optional - attributes will be set to default values if not specified
-                # "DeliveryPolicy": "", # HTTP|HTTPS|Email|Email-JSON|SMS|Amazon SQS|Application|AWS Lambda
-                "DisplayName": "new-pcf-test"
-                # "Policy": "" # dict
-            },
-            "custom_config": {
-                # Refer to https://boto3.amazonaws.com/v1/documentation/api/1.9.5/reference/services/sns.html#SNS.Client.subscribe for a full list of parameters
-                # "Subscription": { # optional
-                #     "Protocol": "", # Required - http|https|email|email-json|sms|sqs|application|lambda
-                #     "Endpoint": "", # http|https|email|email-json|sms|sqs|application|lambda
-                #     "Attributes": {}, # dict
-                #     # "ReturnSubscriptionArn": false # boolean - default=false
-                # }
+    UPDATED_DEF = {
+            "pcf_name": "pcf_sns_test",  # Required
+            "flavor": "sns_topic",  # Required
+            "aws_resource": {
+                # Refer to https://boto3.amazonaws.com/v1/documentation/api/1.9.5/reference/services/sns.html#SNS.Client.create_topic for more information
+                "Name": "pcf-sns-test",  # Required
+                "Attributes": {  # optional - attributes will be set to default values if not specified
+                    # "DeliveryPolicy": "", # HTTP|HTTPS|Email|Email-JSON|SMS|Amazon SQS|Application|AWS Lambda
+                    "DisplayName": "new-pcf-test"
+                    # "Policy": "" # dict
+                },
+                "custom_config": {
+                    # Refer to https://boto3.amazonaws.com/v1/documentation/api/1.9.5/reference/services/sns.html#SNS.Client.subscribe for a full list of parameters
+                    # "Subscription": { # optional
+                    #     "Protocol": "", # Required - http|https|email|email-json|sms|sqs|application|lambda
+                    #     "Endpoint": "", # http|https|email|email-json|sms|sqs|application|lambda
+                    #     "Attributes": {}, # dict
+                    #     # "ReturnSubscriptionArn": false # boolean - default=false
+                    # }
+                }
             }
         }
-    }
 ############################################################################
 
-session = boto3.Session()
-dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, PATH)
-pill = placebo.attach(session, data_path=filename)
-pill.record()
+    def test_apply_states(self):
+        session = boto3.Session()
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, self.PATH)
+        pill = placebo.attach(session, data_path=filename)
+        pill.playback()
 
-particle = PARTICLE(DEF, session)
+        particle = self.PARTICLE(self.DEF, session)
 
-# Test start
+        # Test start
 
-particle.set_desired_state(State.running)
-particle.apply(sync=True)
+        particle.set_desired_state(State.running)
+        particle.apply(sync=True)
 
-print(particle.get_state() == State.running)
-print(particle.is_state_definition_equivalent())
+        print(particle.get_state() == State.running)
+        print(particle.is_state_definition_equivalent())
 
-# Test update
-if UPDATED_DEF:
-    particle = PARTICLE(UPDATED_DEF, session)
-    particle.set_desired_state(State.running)
-    particle.apply(sync=True)
+        assert particle.get_state() == State.running
 
-    print(particle.is_state_definition_equivalent())
+        # Test update
+        if self.UPDATED_DEF:
+            particle = self.PARTICLE(self.UPDATED_DEF, session)
+            particle.set_desired_state(State.running)
+            particle.apply(sync=True)
+            print(particle.is_state_definition_equivalent())
+            assert particle.is_state_definition_equivalent() == True
 
-# Test Terminate
+        # Test Terminate
 
-particle.set_desired_state(State.terminated)
-particle.apply(sync=True)
+        particle.set_desired_state(State.terminated)
+        particle.apply(sync=True)
 
-print(particle.get_state() == State.terminated)
-pill.stop()
+        print(particle.get_state() == State.terminated)
+        assert particle.get_state() == State.terminated
+        pill.stop()
